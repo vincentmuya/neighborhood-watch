ef4efc5c8c12c62fb073d79834e2ab3f6311820e
vincentmuya <vincentmuya13@gmail.com>
2018-03-16 12:34:25 +0300

    created registration form and urls and registration and login template

--- neighborhood/neighborhood/urls.py
+++ neighborhood/neighborhood/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('watch.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+
 ]
