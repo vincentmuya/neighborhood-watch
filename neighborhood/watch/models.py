a60cad9339a7f73da2da6cb8d2a13e9b9eb18803
vincentmuya <vincentmuya13@gmail.com>
2018-03-16 12:43:22 +0300

    created class profile and imported user from database

--- neighborhood/watch/models.py
+++ neighborhood/watch/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    neighborhood_name = models.CharField(max_length=30, blank=True)
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def this_profile(cls):
+        profile = cls.objects.all()
+        return profile
+
+
+def Create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = Profile.objects.create(user=kwargs['instance'])
+
+post_save.connect(Create_profile,sender=User)
